5442a0f74ff6930ff8d07a376e8d2c13189af044
eden1207 <alexis.mauray@gmail.com>
2022-09-21 10:05:40 +0200

    Version 4 de modal.js avec toutes les fonctionnalités ajoutées

--- modal.js
+++ modal.js
@@ -27,18 +27,18 @@ function editNav() {
 // Ma version de DOM Elements
 
 // Class associées au conteneur de la fenêtre "formulaire" et de la fenêtre de confirmation d'inscription
-const modalbg = document.querySelector(".bground");
-const modalConfirmbg = document.querySelector(".confirm-bground");
+const modalbg = document.querySelector(".JS-bground");
+const modalConfirmbg = document.querySelector(".JS-confirm-bground");
 
 // Les boutons
 
 // Boutons d'ouverture de fenêtres (formulaire et confirmation)
-const modalBtn = document.querySelectorAll(".modal-btn");
-const submitBtn = document.getElementById("btn-submit");
+const modalBtn = document.querySelectorAll(".JS-modal-btn");
+const submitBtn = document.getElementById("JS-btn-submit");
 
 // Boutons de fermeture de fenêtre
-const closeBtn = document.querySelectorAll(".close");
-const closeConfirmBtn = document.querySelectorAll(".close-confirm-button");
+const closeBtn = document.querySelectorAll(".JS-close");
+const closeConfirmBtn = document.querySelectorAll(".JS-close-confirm-button");
 
 const formData = document.querySelectorAll(".formData");
 //const confirmbg = document.querySelector(".confirm-window");
@@ -46,18 +46,29 @@ const formData = document.querySelectorAll(".formData");
 
 // Les variables correspondand aux entrées dans le formulaire
 
-const firstname = document.getElementById('first');
-const surname = document.getElementById('last');
-const email = document.getElementById('email');
-const birthdate = document.getElementById('birthdate');
-const NumberofTournament = document.getElementById('quantity');
+const firstname = document.getElementById('JS-first');
+const surname = document.getElementById('JS-last');
+const email = document.getElementById('JS-email');
+const birthdate = document.getElementById('JS-birthdate');
+const NumberofTournament = document.getElementById('JS-quantity');
 
-//const checkIcon1 = document.getElementById('checkbox1:checked');
-//const checkbox = document.querySelector('input[type="checkbox"]:checked');
+const location1 = document.getElementById('JS-location1');
+const location2 = document.getElementById('JS-location2');
+const location3 = document.getElementById('JS-location3');
+const location4 = document.getElementById('JS-location4');
+const location5 = document.getElementById('JS-location5');
+const location6 = document.getElementById('JS-location6');
 
+const checkbox1 = document.getElementById('JS-checkbox1');
 
-// Regex pour vérification du @ dans l'adresse mail
-const ckeckingMail = /@/;
+// RegExp pour vérification de l'adresse mail
+// Le ^ marque le début et $ la fin de l'expression. 
+// Le [a-zA-Z0-9.-_] indique qu'il faut mettre un élément de type lettre d'alphabet, chiffre, ., - ou _ et le "+" indique qu'on peut en mettre plusieurs
+// Le [@]{1} indique qu'il doit y avoir un @
+// Le [a-zA-Z0-9.-_]+ pour suivre l'ensemble avec les caractères indiqués
+// Le [.]{1} pour dire qu'il doit ensuite y avoir un point
+// Enfin le [a-z]{2,3} pour que l'ensemble se termine par 2 à 3 lettres (ex: fr, net, com)
+const ckeckingMail = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,3}$/;
 
 // Ma version de launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -71,64 +82,83 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-//function launchConfirm() {
-  //modalbg.style.display = "none";
-  //if(bla == 0) {
-    //modalConfirmbg.style.display = "block";
-  //}else{
-    //modalbg3.style.display = "block";
-  //}
-//}
+let tabMessages = [
+  {
+    "ErrorNumber" : document.getElementById("JS-error1"),
+    "Error" : "Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
+    "Confirm" : ""
+  },
 
-const radiobox = document.querySelector('input[type="radio"]:checked');
-const checkbox = document.querySelector('input[id="checkbox1"]:checked');
+  {
+    "ErrorNumber" : document.getElementById("JS-error2"),
+    "Error" : "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
+    "Confirm" : ""
+  },
 
+  {
+    "ErrorNumber" : document.getElementById("JS-error3"),
+    "Error" : "Vous devez saisir une adresse valide.",
+    "Confirm" : ""
+  },
+
+  {
+    "ErrorNumber" : document.getElementById("JS-error4"),
+    "Error" : "Vous devez entrer votre date de naissance.",
+    "Confirm" : ""
+  },
+
+  {
+    "ErrorNumber" : document.getElementById("JS-error5"),
+    "Error" : "Vous devez saisir une valeur numérique.",
+    "Confirm" : ""
+  },
+
+  {
+    "ErrorNumber" : document.getElementById("JS-error6"),
+    "Error" : "Vous devez choisir une option.",
+    "Confirm" : ""
+  },
+
+  {
+    "ErrorNumber" : document.getElementById("JS-error7"),
+    "Error" : "Vous devez vérifier que vous acceptez les termes et conditions.",
+    "Confirm" : ""
+  }
+];
+
+class informations {
+  constructor(ErrorName, ErrorMessage, ConfirmMessage){
+    this.ErrorName = ErrorName;
+    this.ErrorMessage = ErrorMessage;
+    this.ConfirmMessage = ConfirmMessage;
+  }
+};
 
 function launchConfirm() {
-  if(firstname.value.length < 2 || surname.value.length < 2 || birthdate.value.length === 0 || ckeckingMail.test(email.value) == false || NumberofTournament.value.length === 0 || radiobox.checked !== true || checkbox.checked !== true){
+  let conditions = [
+    firstname.value.length < 2,
+    surname.value.length < 2,
+    ckeckingMail.test(email.value) == false,
+    birthdate.value.length === 0,
+    NumberofTournament.value.length === 0,
+    location1.checked == false && location2.checked == false && location3.checked == false && location4.checked == false && location5.checked == false && location6.checked == false,
+    checkbox1.checked == false
+  ];
+  if(conditions[0] || conditions[1] || conditions[2] || conditions[3] || conditions[4] || conditions[5] || conditions[6]){  
     errorMessage();
-
-    /*const radioBtn = document.getElementById('location6');*/
-    //const checkbox = document.querySelector('input[id="checkbox1"]:checked');
-    //const checkbox = document.getElementById('checkbox1:checked');
-    //console.log(checkbox.checked);
-
-    console.log('Erreur');
-    //console.log(checkIcon1.ckecked);
   }else{
     modalbg.style.display = "none";
     modalConfirmbg.style.display = "block";
   }
-}
 
-function errorMessage() {
-  if(firstname.value.length < 2){
-    const error1 = document.getElementById("error1");
-    error1.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
-  }
-  if(surname.value.length < 2){
-    const error2 = document.getElementById("error2");
-    error2.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-  }
-  if(ckeckingMail.test(email.value) == false){
-    const error3 = document.getElementById("error3");
-    error3.innerHTML = "Vous devez saisir une adresse valide.";
-  }
-  if(birthdate.value.length === 0){
-    const error4 = document.getElementById("error4");
-    error4.innerHTML = "Vous devez entrer votre date de naissance.";
-  }
-  if(NumberofTournament.value.length === 0){
-    const error5 = document.getElementById("error5");
-    error5.innerHTML = "Vous devez saisir une valeur numérique.";
-  }
-  if(radiobox.checked !== true){
-    const error6 = document.getElementById("error6");
-    error6.innerHTML = "Vous devez choisir une option.";
-  }
-  if(checkbox.checked !== true){
-    const error7 = document.getElementById("error7");
-    error7.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
+  function errorMessage(){
+    for(let i=0; i<7; i++){
+      if(conditions[i]){
+        new informations(tabMessages[i].ErrorNumber, tabMessages[i].Error, tabMessages[i].Confirm).ErrorName.innerHTML = new informations(tabMessages[i].ErrorNumber, tabMessages[i].Error, tabMessages[i].Confirm).ErrorMessage;
+      }else{
+        new informations(tabMessages[i].ErrorNumber, tabMessages[i].Error, tabMessages[i].Confirm).ErrorName.innerHTML = new informations(tabMessages[i].ErrorNumber, tabMessages[i].Error, tabMessages[i].Confirm).ConfirmMessage;
+      }
+    }
   }
 }
 
@@ -137,8 +167,5 @@ function closeWindow() {
   modalConfirmbg.style.display = "none";
 }
 
-//function launchSubmit(){
-//  confirmbg.style.display = "block";
-//}
 
 
